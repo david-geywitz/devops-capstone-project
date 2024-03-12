9878de28474f8dafe2d502c59ed1bb19c36113ee
david-geywitz <56725848+david-geywitz@users.noreply.github.com>
2024-03-12 10:51:07 +0100

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -72,8 +72,9 @@ def list_accounts():
     account_list = [account.serialize() for account in accounts]
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
+    
 
-        ######################################################################
+    ######################################################################
     # READ AN ACCOUNT
     ######################################################################
     @app.route("/accounts/<int:account_id>", methods=["GET"])
